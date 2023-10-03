d152e0423f3d96c6f340bac7fcd77cf28a1c417e
Rajesh Thalla <97143348+Rajeshzealster@users.noreply.github.com>
2023-10-03 14:53:19 +0530

    Update and rename userdata1.sh to userdata_display.sh

--- userdata_display.sh
+++ userdata_display.sh
@@ -8,9 +8,6 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # Install the AWS CLI
 apt install -y awscli
 
-# Download the images from S3 bucket
-#aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
-
 # Create a simple HTML file with the portfolio content and display the images
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
@@ -32,7 +29,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to CloudChamp's Channel</p>
+  <p>Welcome Thalla</p>
   
 </body>
 </html>
